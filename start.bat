402f3fce893e737fb4587aca3805e856c54f882f
Harsh-0192 <iamonly7789@gmail.com>
2026-05-13 11:51:29 +0530

    a

--- start.bat
+++ start.bat
@@ -1,2 +1,2 @@
-cd /d C:\Users\HP\Documents\GitHub\NP-Frontend\server
-npm start
+cd /d C:\Users\HP\Documents\GitHub\NP-Frontend
+cd client && npm i && npm run build && npm i && cd .. && cd server && npm i &&npm start
